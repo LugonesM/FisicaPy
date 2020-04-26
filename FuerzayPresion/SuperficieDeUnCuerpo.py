63c384cd2405847fd73147f8415a045f911f957e
LugonesM <54716002+LugonesM@users.noreply.github.com>
2020-04-26 13:45:03 -0300

    SuperficieDeUnCuerpo.py
    Ejercicio del libro de física: La presión que ejerce sobre una superficie un cuerpo que pesa 1 tonelada es 50 kg/cm²
    ¿Cuanto mide la superficie?.
    En esta ocasión agregue una función que chequea si el input recibió lo necesario para hacer la cuenta, si no(si recibe str), lo intenta cambiar(pasar a float), y si no puede sale del programa.

--- FuerzayPresion/SuperficieDeUnCuerpo.py
+++ FuerzayPresion/SuperficieDeUnCuerpo.py
@@ -1,17 +1,40 @@
-if __name__ == '__main__':
+
+import sys 
+
 #Ejercicio del libro: 
 #La presion que ejerce sobre una superficie un cuerpo que pesa 1 tonelada es 50 kg/cm²
 #¿Cuanto mide la superficie?
 
-  
- print("¿Cuanto mide la superficie de un cuerpo con cierto peso si su presion es tanta?")
+def check( value ) :   #funcion que chequea que el usuario ingreso valores posibles para la cuenta 
+ p = 0
+ try:
+  float(value)
+ except ValueError:  #de no ser asi imprime un mensaje de error y sale del programa
+   print( "pone solo numeros boludoo")
+   sys.exit()
+ return p
 
- peso = float(input("Peso del cuerpo en toneladas: "))
- presion = float(input("presion sobre la base en kg/cm² "))
- 
-#Resulevo usando misma formula presion igual fuerza sobre superficie
- peso = peso * 1000
- superficie = peso / presion
+def strToFloat( cadena ):
+ if type(cadena) == str :
+   cadena = cadena.replace(",",".")  #encuentra las comas en el str y las reemplaza por un punto
+   if check(cadena) == 0 :
+     cadena=float(cadena)
+ return cadena
 
- print("La superficie del cuerpo sera de : ")
- print(superficie, "cm²") #20cm² en ejemplo de libro
+
+if __name__ == '__main__':
+
+ print("¿Cual es el peso especifico de un cubo segun su peso y longitud de su arista (decimales detras de un punto)?")
+
+ a = input("Arista del cubo en cm: ")
+ a = strToFloat(a)
+ peso = input("Su peso en kg: ")
+ peso = strToFloat(peso)  
+
+ peso = peso * 1000           #paso kg a gr
+ volumen = pow(a,3)               #funcion para elevar a x potencia
+ pesoEsp = peso / volumen       #funcion del peso especifico
+
+ print("Su peso especifico sera de : ")
+ print(pesoEsp, "g/cm²")
+sys.exit()
