15a053fbef47536f100f2513d4dc254bd1ee7851
LugonesM <54716002+LugonesM@users.noreply.github.com>
2020-05-02 21:46:48 -0300

    Update diametroEsferadeNiquel.py

--- FuerzayPresion/diametroEsferadeNiquel.py
+++ FuerzayPresion/diametroEsferadeNiquel.py
@@ -9,27 +9,30 @@ PEN = 8.6 #peso especifico del niquel en g/cm³
 
 
 def check( value ) :  
- """Chequea si puede convertir las cadena a float:
-Si puede la convierte y retorna eso, 
-Si no puede cierra el programa con un mensaje de:  pone solo numeros"""
+   """
+   Chequea si puede convertir las cadena a float:
+      Si puede la convierte y retorna eso, 
+      Si no puede cierra el programa con un mensaje de:  pone solo numeros
+   """
  try:
   float(value)
  except ValueError: 
    print( "pone solo numeros")
    sys.exit()
- return 
+  return 
 
 def diametroEsfera(vol):
-""" toma volumen de una esfera (float) y devuelve su diametro (float). Redondea el resultado de la raiz cubica con funcion round"""
+  """ toma volumen de una esfera (float) y devuelve su diametro (float). """
   radio = ((vol * 3) / (math.pi * 4))**(1./3.) #siguiendo formula para sacar volumen de una esfera = 4/3 * pi * radio³
   return radio * 2
 
 
 def strToFloat( cadena ):
   """Toma un avariable, si es de tipo str reemplaza sus comas, si las tiene, por puntos. 
-Chequea si puede convertir las cadena a float( mediante funcion check) : 
-Si puede la convierte y retorna eso, 
-Si no puede cierra el programa con un mensaje"""
+      Chequea si puede convertir las cadena a float( mediante funcion check) : 
+        Si puede la convierte y retorna eso, 
+        Si no puede cierra el programa con un mensaje
+  """
  if type(cadena) == str:
    cadena = cadena.replace(",",".")  #encuentra las comas en el str y las reemplaza por un punto
    if check(cadena) != 1 :
